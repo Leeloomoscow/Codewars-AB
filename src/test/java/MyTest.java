1e5394113b66b31a77bf95195ac611aa864a8006
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-20 15:27:09 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,18 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testSum() throws Exception {
+        byte a = 1;
+        byte b = 2;
+        assertEquals(3, FirstClass.sum(a, b));
+    }
 
+    private static class FirstClass {
+        public static int sum(byte a, byte b) {
+            return a + b;
+        }
     }
 }
